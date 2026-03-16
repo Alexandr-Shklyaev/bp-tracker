dbf6adb07189b5c9592d4d30bf4a10622f09af8f
Alexandr-Shklyaev <146367324+Alexandr-Shklyaev@users.noreply.github.com>
2026-03-16 22:08:02 +0400

    Initial Commit

---
+++ bp-tracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  bp-tracker
+//
+//  Created by Alexandr Shklyaev on 16.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
